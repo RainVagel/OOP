f026a0f22f07124af5b4cb991f95c8703b1fda86
rainvagel <rain.vagel@gmail.com>
2015-03-31 03:19:39 +0300

    Automaatne sudoku lahendaja!

---
+++ src/Lahendaja.java
@@ -0,0 +1,181 @@
+import java.util.ArrayList;
+
+/**
+ * Created by rainvagel on 27/03/15.
+ */
+
+class Lahendaja {
+    static int[][] sudoku = {{6,7,1,0,0,2,0,3,0},{0,2,4,0,0,7,0,9,1},{0,0,0,1,8,6,0,4,7},
+            {0,3,8,4,0,0,9,1,0},{1,0,0,3,9,8,0,0,2},{4,0,2,0,6,0,3,0,8},{9,0,0,2,7,0,5,8,0},
+            {2,4,0,8,0,0,1,6,0},{0,8,3,0,1,9,0,0,4}};
+
+
+    static boolean vaatlejaVerikaalne(int y, int number) {
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[i][y] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+    static boolean vaatlejaHoristonaalne(int x, int number) {
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[x][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean vaatlejaKast(int x, int y, int number) {
+        for (int i = 0; i < getVäikeKast(x, y).length;i++) {
+            if (getVäikeKast(x, y)[i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean onVahel (int arv1, int arv2, int arv3) {
+        if (arv1 <= arv3 && arv1 >= arv2) {
+            return true;
+        }
+        return false;
+    }
+    static int[] getVäikeKast(int x, int y) {
+        int[] kast = new int[9];
+        if (onVahel(x,0,2) && onVahel(y,0,2)) {
+            return väikeseLoomine(0,2,0,2);
+        }
+        else if (onVahel(x,3,5) && onVahel(y,0,2)) {
+            return väikeseLoomine(3,5,0,2);
+        }
+        else if (onVahel(x,6,8) && onVahel(y,0,2)) {
+            return väikeseLoomine(6,8,0,2);
+        }
+        else if (onVahel(x,0,2) && onVahel(y,3,5)) {
+            return väikeseLoomine(0,2,3,5);
+        }
+        else if (onVahel(x,0,2) && onVahel(y,6,8)) {
+            return väikeseLoomine(0,2,6,8);
+        }
+        else if (onVahel(x,3,5) && onVahel(y,3,5)) {
+            return väikeseLoomine(3,5,3,5);
+        }
+        else if (onVahel(x,3,5) && onVahel(y,6,8)) {
+            return väikeseLoomine(3,5,6,8);
+        }
+        else if (onVahel(x,6,8) && onVahel(y,3,5)) {
+            return väikeseLoomine(6,8,3,5);
+        }
+        else{
+            return väikeseLoomine(6,8,6,8);
+        }
+    }
+
+    static int[] väikeseLoomine(int a, int b, int c, int d) {
+        int h = 0;
+        int[] kast = new int[9];
+        for (int i = a; i < b; i++) {
+            for (int j = c; j < d; j++) {
+                kast[c] = sudoku[i][j];
+            }
+        }
+        return kast;
+    }
+
+    static boolean sobilik(VäikeRuut väikene, int num) {
+        if (sudoku[väikene.getRida()][väikene.getVeerg()] != 0) {
+            throw new RuntimeException("Juba on väärtus!");
+        }
+        if (vaatlejaHoristonaalne(väikene.getRida(), num)) {
+            return false;
+        } else if (vaatlejaVerikaalne(väikene.getVeerg(), num)) {
+            return false;
+        } else if (vaatlejaKast(väikene.getRida(), väikene.getVeerg(), num)) {
+            return false;
+        }
+        return true;
+    }
+
+    static VäikeRuut getJärgmineVäikeRuut(VäikeRuut hetkel) {
+        int rida = hetkel.rida;
+        int veerg = hetkel.veerg;
+
+        veerg++;
+
+        if (veerg > 8) { //Liigub järgmisele reale
+            veerg = 0;
+            rida++;
+        }
+        if (rida > 8) { //Siin saab läbi
+            return null;
+        }
+
+        VäikeRuut järgmine = new VäikeRuut(rida, veerg);
+        return järgmine;
+    }
+
+    static boolean lahenda(VäikeRuut hetkel) {
+        if (hetkel == null) {
+            return true;
+        }
+        if (sudoku[hetkel.rida][hetkel.veerg] != 0){
+            return lahenda(getJärgmineVäikeRuut(hetkel));
+        }
+        for (int i = 1; i <= 9; i++) {
+            boolean valid = sobilik(hetkel, i);
+            if (!valid) {
+                continue;
+            }
+            sudoku[hetkel.getRida()][hetkel.getVeerg()] = i;
+            boolean lahendatud = lahenda(getJärgmineVäikeRuut(hetkel));
+            if (lahendatud) {
+                return true;
+            }
+            else {
+                sudoku[hetkel.getRida()][hetkel.getVeerg()] = 0;
+            }
+        }
+        return false;
+    }
+
+    static void väljasta(int[][] sudoku) {
+        int x = 0;
+        for (int i = 0; i < 9;i++) {
+            for (int j = 0; j < 9;j++) {
+                System.out.print("|" + sudoku[i][j] + "|");
+                if (j % 3 == 2) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println("");
+            if (x % 3 == 2 && i != 9 - 1) {
+                for (int j = 0; j < 9*3 + 3;j++) {
+                    System.out.print("-");
+                }
+                System.out.println("");
+            }
+            for (int j = 0; j < 9*3 + 3;j++) {
+                System.out.print("-");
+            }
+            System.out.println("");
+            x++;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Algne");
+        väljasta(sudoku);
+        System.out.println();
+        boolean lahendatud = lahenda(new VäikeRuut(0,0));
+        if (!lahendatud) {
+            System.out.println("Ei saa lahendada!");
+        }
+        else {
+            System.out.println("Lahendatud!");
+            väljasta(sudoku);
+        }
+    }
+}
---
+++ src/VäikeRuut.java
@@ -0,0 +1,19 @@
+/**
+ * Created by rainvagel on 31/03/15.
+ */
+class VäikeRuut {
+    int rida, veerg;
+
+    public VäikeRuut(int rida, int veerg) {
+        this.rida = rida;
+        this.veerg = veerg;
+    }
+
+    public int getRida() {
+        return rida;
+    }
+
+    public int getVeerg() {
+        return veerg;
+    }
+}
